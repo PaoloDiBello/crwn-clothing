999c38108e38e92313552c645e3b058951c2a27b
PaoloDiBello <dibellopol@gmail.com>
2019-07-28 11:58:50 +0200

    Update shop data with new data, update cart utils to group items in cart reducer

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -3,3 +3,13 @@ export const toggleCartHidden = () => ({
     type: cartActionTypes.TOGGLE_CART_HIDDEN,
 });
 
+export const addItem = item => ({
+    type: cartActionTypes.ADD_ITEM,
+    payload: item
+});
+
+export const deleteItem = item => ({
+    type: cartActionTypes.DELETE_ITEM,
+    payload: item
+});
+
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,7 +1,10 @@
 import { cartActionTypes } from './cart.types'
+import { addItemToCart } from './cart.utils'
+
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cartItems: []
 }
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
@@ -12,9 +15,22 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                 hidden: !state.hidden
             }
 
+        case cartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cartItems: addItemToCart(state.cartItems, action.payload)
+            }
+
+        case cartActionTypes.REMOVE_ITEM:
+            return {
+                ...state,
+                cartItems: state.cartItems.filter(item => item !== action.payload)
+            }
+
         default:
             return state;
     }
+
 }
 
 export default cartReducer;
