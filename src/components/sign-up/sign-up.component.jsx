ba55fda1558aecdae38f001472343d90e7f35da3
PaoloDiBello <dibellopol@gmail.com>
2019-08-15 00:31:42 +0200

    created sign up sagas

--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -3,9 +3,12 @@ import React from 'react';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
-
 import { SignUpContainer, SignUpTitle } from './sign-up.styles';
+
+import { signUpStart } from '../../redux/user/user.actions'
+
+import { connect } from 'react-redux'
+
 
 class SignUp extends React.Component {
     constructor() {
@@ -21,6 +24,7 @@ class SignUp extends React.Component {
 
     handleSubmit = async event => {
         event.preventDefault();
+        const { signUpStart } = this.props;
 
         const { displayName, email, password, confirmPassword } = this.state;
 
@@ -28,24 +32,7 @@ class SignUp extends React.Component {
             alert("passwords don't match");
             return;
         }
-
-        try {
-            const { user } = await auth.createUserWithEmailAndPassword(
-                email,
-                password
-            );
-
-            await createUserProfileDocument(user, { displayName });
-
-            this.setState({
-                displayName: '',
-                email: '',
-                password: '',
-                confirmPassword: ''
-            });
-        } catch (error) {
-            console.error(error);
-        }
+        signUpStart({ displayName, email, password })
     };
 
     handleChange = event => {
@@ -100,4 +87,10 @@ class SignUp extends React.Component {
     }
 }
 
-export default SignUp;
+
+const mapDispatchToProps = dispatch => ({
+    signUpStart: (user) => dispatch(signUpStart(user))
+})
+
+
+export default connect(null, mapDispatchToProps)(SignUp);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -4,6 +4,7 @@ const INITIAL_STATE = {
     currentUser: null,
     error: null
 }
+
 const userReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
 
@@ -16,6 +17,7 @@ const userReducer = (state = INITIAL_STATE, action) => {
 
         case userActionTypes.SIGN_IN_FAILURE:
         case userActionTypes.SIGN_OUT_FAILURE:
+        case userActionTypes.SIGN_UP_FAILURE:
             return {
                 ...state,
                 error: action.payload
