22baf4eef1641ce61bb2325c124600b0e73d5384
PaoloDiBello <dibellopol@gmail.com>
2019-08-24 18:31:20 +0200

    Add lazy loading

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { GlobalStyle } from './global.styles'
-import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import CheckoutPage from './pages/checkout/checkout.component'
 
 import { Switch, Route, Redirect } from 'react-router-dom'
 
@@ -11,10 +7,17 @@ import { connect } from 'react-redux'
 import { createStructuredSelector } from 'reselect'
 
 import Header from "./components/header/header.component";
+import Spinner from './components/spinner/spinner.component.jsx'
 
 import { checkUserSession } from './redux/user/user.actions'
 
 import { selectCurrentUser } from './redux/user/user.selector'
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const ShopPage = lazy(() => import('./pages/shop/shop.component'));
+const SignInAndSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
+const CheckoutPage = lazy(() => import('./pages/checkout/checkout.component'));
+
 
 const App = ({ currentUser, checkUserSession }) => {
 
@@ -28,15 +31,17 @@ const App = ({ currentUser, checkUserSession }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/checkout' component={CheckoutPage} />
-        <Route exact path="/signin" render={() =>
-          currentUser ? (<Redirect to='/' />
-          ) : (
-              <SignInAndSignUpPage />
-            )
-        } />
+        <Suspense fallback={<Spinner />}>
+          <Route exact path="/" component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/checkout' component={CheckoutPage} />
+          <Route exact path="/signin" render={() =>
+            currentUser ? (<Redirect to='/' />
+            ) : (
+                <SignInAndSignUpPage />
+              )
+          } />
+        </Suspense>
       </Switch>
     </div>
   );
--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -1,18 +1,12 @@
 
 import React from 'react'
 
-import { SpinnerOverlay, SpinnerContainer } from './with-spinner.styles';
+import Spinner from '../spinner/spinner.component'
 
-const WithSpinner = WrappedComponent => {
-
-    const Spinner = ({ isLoading, ...restProps }) => {
-        return isLoading ? (
-            <SpinnerOverlay>
-                <SpinnerContainer />
-            </SpinnerOverlay>
-        ) : (<WrappedComponent {...restProps} />)
-    }
-    return Spinner
+const WithSpinner = WrappedComponent => ({ isLoading, ...restProps }) => {
+    return isLoading ? (<Spinner />) : (<WrappedComponent {...restProps} />)
 }
+
+
 export default WithSpinner
 
